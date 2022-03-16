540002de5f8c278a4ab1920fe91cf3bad0fd15de
AxelUrizar <axelurizarmartin@gmail.com>
2022-03-16 14:58:13 +0000

    rutas Users finished

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ require('./config/mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var tokensRouter = require('./routes/tokens');
 var rolesRouter = require('./routes/roles');
 var gamesHistoryRouter = require('./routes/gamesHistory');
 
@@ -31,6 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/roles', rolesRouter)
+app.use('/gamesHistory', gamesHistoryRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
